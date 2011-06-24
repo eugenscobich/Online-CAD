ae00c35f64e9d9489edbc57a728a120ca916e0e5
Eugen Scobich <eugen.scobich@mail.ru>
2011-06-24 12:41:41 +0300

    configured dao layer project

---
+++ source/OnlineCADDAOLayer/src/main/java/org/onlinecad/dao/LayerDAO.java
@@ -0,0 +1,33 @@
+package org.onlinecad.dao;
+
+import java.io.Serializable;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.onlinecad.model.entity.Layer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("layerDAO")
+public class LayerDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	public Session getSession() {
+		return sessionFactory.getCurrentSession();
+	}
+
+	public void save(Object entity) {
+		getSession().save(entity);
+		getSession().flush();
+	}
+
+	public Layer get(Serializable id) {
+		return (Layer) getSession().get(Layer.class, id);
+	}
+
+	public Layer load(Serializable id) {
+		return (Layer) getSession().load(Layer.class, id);
+	}
+}
---
+++ source/OnlineCADDAOLayer/src/test/java/org/onlinecad/dao/SimpleDAOTest.java
@@ -0,0 +1,28 @@
+package org.onlinecad.dao;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.onlinecad.model.entity.Layer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@ContextConfiguration(locations = { "classpath:/spring/dao-context.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SimpleDAOTest {
+
+	@Autowired
+	private LayerDAO layerDAO;
+
+	@Test
+	@Transactional
+	public void testSave() {
+		Layer layer = new Layer();
+		layer.setAnswer("dfsdfsd");
+		layerDAO.save(layer);
+		assertNotNull(layer.getId());
+	}
+}

6eaa7adf2751791af94308f7c73d1331add7c057
Eugen Scobich <eugen.scobich@mail.ru>
2011-06-24 11:49:12 +0300

    configure model layer projects

--- source/OnlineCADModelLayer/src/main/java/org/onlinecad/model/entity/Layer.java
+++ source/OnlineCADModelLayer/src/main/java/org/onlinecad/model/entity/Layer.java
@@ -1,5 +1,36 @@
 package org.onlinecad.model.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Layer")
 public class Layer {
-	
+	@Id
+	@GeneratedValue
+	@Column(name = "Id")
+	private Integer id;
+
+	@Column(name = "Answer")
+	private String answer;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
 }
---
+++ source/OnlineCADModelLayer/src/test/java/org/onlinecad/model/SimplePersistenceTest.java
@@ -0,0 +1,29 @@
+package org.onlinecad.model;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.hibernate.SessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.onlinecad.model.entity.Layer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@ContextConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SimplePersistenceTest {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Test
+	@Transactional
+	public void testAddTechnology() {
+		Layer layer = new Layer();
+		layer.setAnswer("dfsdfsd");
+		sessionFactory.getCurrentSession().save(layer);
+		assertNotNull(layer.getId());
+	}
+}
